f7cedce00d3b8d452cb1f6e2eb26566c3f717c32
makhloufi200 <makhloufi200@yahoo.fr>
2019-06-25 14:13:32 +0200

    Update mymodule.py

--- mymodule.py
+++ mymodule.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 def say_hello(name):
     print("hello "+name)
 
 say_hello("smail")
-
-||||||| merged common ancestors
-=======
-def say_hello():
-  print("hello world")
-
-say_hello()
->>>>>>> c960904eb6a253d7d7d4757d8fa1558575733992
